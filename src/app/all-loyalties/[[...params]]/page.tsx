19af66ec78a7661a608ad5c76973be350f67f991
Karabesk <kari.tu99@gmail.com>
2025-11-10 15:20:24 +0200

    fixed titles for filter-bonus/[]/page all-loyalties/[]/page

--- src/app/all-loyalties/[[...params]]/page.tsx
+++ src/app/all-loyalties/[[...params]]/page.tsx
@@ -1,16 +1,36 @@
 import type { Metadata } from 'next'
+import { LOYALTIECATEGORYIES } from '@/helper'
 import AllLoyaltiesClient from '@/app/all-loyalties/[[...params]]/AllLoyaltiesClient'
 
-export const metadata: Metadata = {
-    title: 'List of best loyalty programs for VIP players | inGamble',
-    description: 'We collected and observed all levels and features of each casinos loyalty program. Find best loyalties and perks for loyal players',
+
+export async function generateMetadata(
+    { params }: { params: { params?: string[] } }
+): Promise<Metadata> {
+    try {
+        const slug = params?.params?.[0]
+        console.log('generateMetadata: slug:', slug)
+        
+        // Get name from LOYALTIECATEGORYIES array
+        const name = LOYALTIECATEGORYIES.find(item => item.slug === slug)?.name || 'Essential VIP Loyalty Programs'
+        console.log('generateMetadata: name from LOYALTIECATEGORYIES:', name)
+
+        return {
+            title: `${name} | inGamble`,
+            description: 'Find the best casino VIP programs and loyalty rewards.',
+        }
+    } catch (error) {
+        console.error('Metadata generation error:', error)
+        return {
+            title: 'Essential VIP Loyalty Programs | inGamble',
+            description: 'Find the best casino VIP programs and loyalty rewards.',
+        }
+    }
 }
 
 const AllLoyalties = async ({ params }: { params: Promise<{ params: string[] }> }) => {
     const dataparam = await params
-    
-    // Передаємо перший параметр як loyaltie_slug
     const loyaltieSlug = dataparam?.params?.[0] || null
+    console.log('AllLoyalties component: loyaltieSlug:', loyaltieSlug)
 
     return <AllLoyaltiesClient loyaltieSlug={loyaltieSlug} />
 }
